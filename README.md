445d862d85a2e98d0d67a1c65307731951913d02
Yoshiki Shibukawa <yoshiki@shibu.jp>
2018-10-10 02:02:34 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-next-ts-materialui [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Next.js + TypeScript + Material UI
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-next-ts-materialui using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-next-ts-materialui
+```
+
+Then generate your new project:
+
+```bash
+yo next-ts-materialui
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+Apache-2.0 © [Yoshiki Shibukawa](https://github.com/shibukawa)
+
+
+[npm-image]: https://badge.fury.io/js/generator-next-ts-materialui.svg
+[npm-url]: https://npmjs.org/package/generator-next-ts-materialui
+[travis-image]: https://travis-ci.org/shibukawa/generator-next-ts-materialui.svg?branch=master
+[travis-url]: https://travis-ci.org/shibukawa/generator-next-ts-materialui
+[daviddm-image]: https://david-dm.org/shibukawa/generator-next-ts-materialui.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/shibukawa/generator-next-ts-materialui
---
+++ generators/app/index.ts
@@ -0,0 +1,134 @@
+import { readFileSync } from "fs";
+import { basename, join } from "path";
+
+import chalk from "chalk";
+import { getLoginUserProfile } from "login-user-profile";
+import * as _ from "underscore.string";
+import Generator from "yeoman-generator";
+import yosay from "yosay";
+
+interface IPackage {
+    version: string;
+    name: string;
+}
+
+function readPackage(): IPackage {
+    const packageJson = JSON.parse(
+        readFileSync(join(__dirname, "..", "..", "package.json"), {
+            encoding: "utf8"
+        })
+    );
+    return packageJson;
+}
+
+export default class extends Generator {
+    /**
+     * Gather basic project information.
+     */
+    public async gatherProjectInfo() {
+        const { version, name } = readPackage();
+        const generatorTitle = `${name} v${version}`;
+        this.log(
+            yosay(
+                `Typescript Project Generators.\n${chalk.red(generatorTitle)} `
+            )
+        );
+
+        const templateTypePrompt = {
+            type: "list",
+            name: "templateType",
+            message: "What type of project do you want to create?",
+            choices: ["nextjs", "node-lib", "universal-lib"],
+            default: "node-lib"
+        };
+
+        const moduleNamePrompt = {
+            name: "name",
+            message: "Module Name",
+            default: basename(process.cwd())
+        };
+
+        const descriptionPrompt = {
+            name: "description",
+            message: "Description",
+            default: "The best web application ever."
+        };
+
+        const homepagePrompt = {
+            name: "homepage",
+            message: "Homepage",
+            default: ""
+        };
+
+        const licensePrompt = {
+            name: "license",
+            message: "License",
+            default: "MIT"
+        };
+
+        const githubUsernamePrompt = {
+            name: "githubUsername",
+            message: "GitHub username",
+            default: ""
+        };
+
+        const authorNamePrompt = {
+            name: "authorName",
+            message: "Author's Name",
+            default: ""
+        };
+
+        const authorEmailPrompt = {
+            name: "authorEmail",
+            message: "Author's Email",
+            default: ""
+        };
+
+        const authorUrlPrompt = {
+            name: "authorUrl",
+            message: "Author's Homepage",
+            default: ""
+        };
+
+        const prompts = [
+            templateTypePrompt,
+            moduleNamePrompt,
+            descriptionPrompt,
+            homepagePrompt,
+            licensePrompt,
+            githubUsernamePrompt,
+            authorNamePrompt,
+            authorEmailPrompt,
+            authorUrlPrompt
+        ];
+
+        const profile = await getLoginUserProfile();
+        if (profile) {
+            if (profile.name) {
+                authorNamePrompt.default = profile.name;
+            }
+            if (profile.email) {
+                authorEmailPrompt.default = profile.email;
+            }
+            if (profile.url) {
+                authorUrlPrompt.default = profile.url;
+            }
+            if (profile.githubUserId) {
+                githubUsernamePrompt.default = profile.githubUserId;
+            }
+        }
+
+        const props = await this.prompt(prompts);
+        props.slugname = _.slugify(props.name);
+        props.camelname = _.camelize(props.name);
+
+        props.repoUrl =
+            "https://github.com/" + props.githubUsername + "/" + props.slugname;
+
+        if (!props.homepage) {
+            props.homepage = props.repoUrl;
+        }
+        const generatorName = "@shibukawa/typescript";
+        this.composeWith(`${generatorName}:${props.templateType}`, props);
+    }
+}
